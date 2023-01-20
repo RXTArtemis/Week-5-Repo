9363e66036d9e0bf5c2ffcd1f6324703b60e6105
RXTArtemis <r_nava20@outlook.com>
2023-01-19 20:41:02 -0600

    more changes

--- index.js
+++ index.js
@@ -14,12 +14,20 @@ class Practice{
         this.doctors= [];
     }
     addDoctor(doctor){
-        if(doctors instanceof Doctor){
-this.doctors.push(doctor);
+     if(doctors instanceof Doctor){
+        this.doctors.push(doctor);
         }else{
             throw new Error(`You can only add instance of Doctor. Argument is not a doctor: ${doctors}. Try again.`);
         }
     }
+    addPractice(officeName){
+        if(officeName instanceof Practice){
+            this.practices.push(officeName);
+        }else{
+            alert('Function not permitted. Only add instance of Practice.');
+        }
+    }
+
     describe(){
         return `${this.name} is a part of a practice of ${this.doctors.length} doctors.`;
     }
@@ -41,10 +49,10 @@ class Menu{
                     this.viewPractice();
                     break;
                 case '3':
-                    this.removePractice();
+                    this.deletePractice();
                     break;
                 case '4':
-                    this.displayPractice();
+                    this.displayAllPractices();
                     break;
                 default:
                     selection = 0;       
@@ -53,55 +61,49 @@ class Menu{
         }
         alert('Goodbye! Have a great day!');
     }
-
     showMainMenuOptions(){
         return prompt(`
         0)exit
-        1)create a new practice
+        1)create new practice
         2)view practice
-        3)remove practice
+        3)delete practice
         4)display all practices
         `);
     }
 
     showPracticeMenuOptions(practiceInfo){
         return prompt(`
-        0) back
+        0)back
         1)create doctor
-        2)delete doctor
+        2)remove doctor
         ------------------
         ${practiceInfo}
         `);
     }
-
-    displayPractice(){
-        let practiceString =this.selectedPractice.officeName;
+    displayAllPractices(){
+        let practiceString = " ";
         for(let i = 0; i < this.practices.length; i++){
-        practiceString += i + `) ` + this.selectedPractice[i].officeName + '\n';
+        practiceString += i + `) ` + this.practices[i].officeName + '\n';
         }
         alert(practiceString);
-        console.log(practiceString)
-    }
-
+        }
+    
     createNewPractice(){
         let name = prompt('Enter Practice Name.');
         this.practices.push(new Practice(name));
-        console.log(this.practices)
-        console.log(Practice.officeName)
     }
 
-   viewPractice(){
+    viewPractice(){
     let index= prompt('Enter the index of the practice you would like to see.');
-
     if(index > -1 && index < this.practices.length){
       this.selectedPractice=this.practices[index]; 
-      let description = "Practice Name:" + this.selectedPractice.officeName + '\n';
-      console.log(this.selectedPractice)
+      let description = `Practice Name:${this.selectedPractice.officeName}\n`;
       
       for(let i = 0; i < this.selectedPractice.doctors.length; i++){
         description += i + ') ' + this.selectedPractice.doctors[i].name 
         + '-'+ this.selectedPractice.doctors[i].specialty + '\n';
       }
+
       let selection = this.showPracticeMenuOptions(description);
       switch(selection){
         case '1':
@@ -111,26 +113,25 @@ class Menu{
             this.removeDoctor();
       }
     }
-  }
-  deletePractice(){
+}
+deletePractice(){
     let index = prompt('Enter the index of the practice you wish to remove.');
     if(index> -1 && index < this.practices.length){
         this.practices.splice(index, 1);
-    }
-} 
+             }
+         }
 
-   createNewDoctor(){
+ createNewDoctor(){
     let name = prompt("Enter name for new doctor.");
     let specialty = prompt("Enter new specialty.");
     this.selectedPractice.doctors.push(new Doctor(name,specialty));
-   }
-
+    }
    removeDoctor(){
     let index = prompt('Enter the index of doctor you would like to remove.');
     if(index > -1 && index < this.selectedPractice.doctors.length){
         this.selectedPractice.doctors.splice(index, 1);
+        }
     }
-   }
 }
 
 let menu = new Menu();
